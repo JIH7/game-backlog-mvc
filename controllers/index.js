048c4a163866b01e953244fce9cbeeebb3e0b1d8
Jeremy Helsel <JIHlucky7@gmail.com>
2023-02-22 18:38:26 -0600

    Added style, tweaked views, added favicon

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,13 @@
 module.exports = {
-    getIndex: (req, res)=>{
+    getIndex: (req,res)=>{
         res.render('index.ejs')
-    }
+    },
+
+    getLogin: (req,res)=>{
+        res.render('loginPage.ejs')
+    },
+
+    getSignup: (req,res)=>{
+        res.render('signupPage.ejs')
+    },
 }
--- routers/index.js
+++ routers/index.js
@@ -3,5 +3,7 @@ const router = express.Router()
 const indexController = require('../controllers/index')
 
 router.get('/', indexController.getIndex)
+router.get('/login', indexController.getLogin)
+router.get('/signup', indexController.getSignup)
 
 module.exports = router
