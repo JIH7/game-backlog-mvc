228b17ec1399de49671a36b2181655d75a1c5262
Jeremy Helsel <JIHlucky7@gmail.com>
2023-02-24 15:09:18 -0600

    Redoing auth progress based on Leon's

--- controllers/index.js
+++ controllers/index.js
@@ -1,13 +1,7 @@
+const user = require('../models/user')
+
 module.exports = {
     getIndex: (req,res)=>{
         res.render('index.ejs')
-    },
-
-    getLogin: (req,res)=>{
-        res.render('loginPage.ejs')
-    },
-
-    getSignup: (req,res)=>{
-        res.render('signupPage.ejs')
     },
 }
